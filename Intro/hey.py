17e13655b9c83a69a05eab0ef3bb2977bd9ffaac
SamuelWyck <samawyck@gmail.com>
2024-10-07 09:34:48 -0500

    Create hey.py

---
+++ Intro/hey.py
@@ -0,0 +1,8 @@
+"""Write a function greet that takes in a string argument, s, and returns the string "hey s". No tricks here. Run the tests to check your work."""
+
+def greet(s):
+  return f"hey {s}"
+
+greet("alvin") # -> "hey alvin"
+greet("jason") # -> "hey jason"
+greet("how now brown cow") # -> 'hey how now brown cow'
